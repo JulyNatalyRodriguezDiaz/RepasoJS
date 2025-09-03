06f92566a12a03408e0e98879378394c35b1e000
julyrodriguez <julynata1999@gmail.com>
2025-09-02 20:16:53 -0500

    repasojs

---
+++ holamundo.js
@@ -0,0 +1,29 @@
+console.log('Hola Mundo');
+
+let cajadeAndy = "Woody"
+
+console.log(cajadeAndy);
+
+let edad = 25;
+let nombre = "July";
+
+console.log("Mi nombre es " + nombre + " y tengo " + edad + " años.");
+
+//array
+
+let frutas = ['manzana', 'banano', 'mango'];
+
+console.log(frutas);
+
+console.log(frutas[1]);
+
+let frase = "Java Scrip es extremadamente genial";
+console.log(frase);
+console.log(frase.length);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
+
+// operaciones matematicas
+const suma = 3 + 12;
+
+console.log(suma);
